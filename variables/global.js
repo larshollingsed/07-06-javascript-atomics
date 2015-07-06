085f57d9f3362e658b3c1af6034f82ecb7df8044
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 11:52:58 -0500

    3 - Variables

---
+++ variables/global.js
@@ -0,0 +1,9 @@
+var name = "Larz", age = 31, hairColor = "Silver Fox";
+
+alert("My name is " + name + ".");
+alert("I am " + age + " years old.");
+alert("The color of my hair makes all the guys call me a " + hairColor + ".");
+
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("The color of my hair makes all the guys call me a " + hairColor + ".");
