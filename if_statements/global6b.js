93b96337845865cb4ecd2b004f2145f04c5896ad
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 14:41:35 -0500

    6B - Password Game

---
+++ if_statements/global6b.js
@@ -0,0 +1,10 @@
+var password = "puppyBreath";
+
+user_input = prompt("Enter the password");
+
+if (password == user_input) {
+  alert("Password is correct!")
+}
+if (password != user_input) {
+  alert ("Swing and a miss on the password buddy :(")
+}
