ed1393faf9f7e15614641476fd30a740ee8b97f9
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 11:19:33 -0500

    2 - Output

---
+++ output/global.js
@@ -0,0 +1,9 @@
+// pops up an alert window as soon at the script is loaded
+alert("Hello, my name is Lars");
+
+// \n(<- that's a backslash) provides a line break in the console
+// console only seems able to be seen when opted into by opening in browser
+console.log("My favorite breakfast is hashbrowns\nI, unfortunately, don't live with enough cats\nI love to chow down at the Whole Foods salad bar\nIn addition, I also don't take enough naps weekly\nIf I had my druthers I would take at least one nap a day, possibly two on sundays\n");
+
+// after the above lines are displayed in the console, this pops up an alert
+alert("Smell ya later");
