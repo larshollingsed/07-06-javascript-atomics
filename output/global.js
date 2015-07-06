780d397c1902b677981384eae1e2caef4e32b62e
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 11:23:42 -0500

    /n vs. multiple console.log() lines

--- output/global.js
+++ output/global.js
@@ -3,7 +3,15 @@ alert("Hello, my name is Lars");
 
 // \n(<- that's a backslash) provides a line break in the console
 // console only seems able to be seen when opted into by opening in browser
-console.log("My favorite breakfast is hashbrowns\nI, unfortunately, don't live with enough cats\nI love to chow down at the Whole Foods salad bar\nIn addition, I also don't take enough naps weekly\nIf I had my druthers I would take at least one nap a day, possibly two on sundays\n");
+// console.log("My favorite breakfast is hashbrowns\nI, unfortunately, don't live with enough cats\nI love to chow down at the Whole Foods salad bar\nIn addition, I also don't take enough naps weekly\nIf I had my druthers I would take at least one nap a day, possibly two on Sundays\n");
+
+// doesn't seem to be much different besides displaying which line of code
+// in this .js each console.log() came from
+console.log("My favorite breakfast is hashbrowns");
+console.log("I, unfortunately, don't live with enough cats");
+console.log("I love to chow down at the Whole Foods salad bar");
+console.log("In addition, I also don't take enough naps");
+console.log("If I had my druthers, I would take at least one nap a day, possibly two on Sundays")
 
 // after the above lines are displayed in the console, this pops up an alert
 alert("Smell ya later");
