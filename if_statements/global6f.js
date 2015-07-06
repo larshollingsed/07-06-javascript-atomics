59f219935530b465fa53a86515d3fd5ab8031a32
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 16:05:04 -0500

    Fixed return to user in case of 0

--- if_statements/global6f.js
+++ if_statements/global6f.js
@@ -4,6 +4,8 @@ var side3 = parseInt(prompt("Enter the length of side 3"));
 
 if (isNaN(side1) == true || isNaN(side2) == true || isNaN(side3) == true) {
   alert("At least one of your entries was not a valid number")
+} else if (side1 == 0 || side2 == 0 || side3 == 0){
+  alert("Are you sure you meant to enter 0?")
 } else if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
   alert("This is a valid triangle")
 } else {
