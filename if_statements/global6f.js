fd32dc6be388bd5bd547c56bbba40e176a6f4aff
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 15:59:08 -0500

    Fixed return to user in case of NaN

---
+++ if_statements/global6f.js
@@ -0,0 +1,11 @@
+var side1 = parseInt(prompt("Enter the length of side 1"));
+var side2 = parseInt(prompt("Enter the length of side 2"));
+var side3 = parseInt(prompt("Enter the length of side 3"));
+
+if (isNaN(side1) == true || isNaN(side2) == true || isNaN(side3) == true) {
+  alert("At least one of your entries was not a valid number")
+} else if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+  alert("This is a valid triangle")
+} else {
+  alert("This is not a valid triangle")
+}
