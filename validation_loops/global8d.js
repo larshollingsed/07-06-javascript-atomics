4d939c035d93afcb92dbd05e686f3bcd138f9900
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-07 09:44:12 -0500

    8D - Triangles, Pt 2

---
+++ validation_loops/global8d.js
@@ -0,0 +1,19 @@
+var valid = false
+
+while (valid === false) {
+  var side1 = parseInt(prompt("Enter the length of side 1"));
+  var side2 = parseInt(prompt("Enter the length of side 2"));
+  var side3 = parseInt(prompt("Enter the length of side 3"));
+  if (isNaN(side1) == true || isNaN(side2) == true || isNaN(side3) == true) {
+      alert("At least one of your entries was not a valid number")
+  } else if (side1 == 0 || side2 == 0 || side3 == 0){
+      alert("Are you sure you meant to enter 0?")
+  } else if (side1 < 0 || side2 < 0 || side3 < 0) {
+      alert("Sides of a triangle can't be negative...at least in this universe")  
+  } else if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+      var valid = true
+      alert("This is a valid triangle")
+  } else {
+      alert("This is not a valid triangle")
+  }
+}
