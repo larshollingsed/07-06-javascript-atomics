a1c395526f8d35941a4ce999fbd45b38e627be5a
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-06 22:11:04 -0500

    8A - What's the Password?

--- validation_loops/global8a.js
+++ validation_loops/global8a.js
@@ -3,3 +3,9 @@ var input
 while (password != input) {
   var input = prompt("Enter the password")
 }
+
+// using a "for" loop
+// var input
+// for (password = "sushi"; password != input; input) {
+//   var input = prompt("Enter the password")
+// }
